85e0e1e22af7c7c206d228304beab8116e5a5a20
Xenon3099 <dmamritesh@gmail.com>
2019-06-19 19:20:51 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,8 +19,9 @@ bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
-	bot.user.setActivity("Game");
 });
+
+bot.user.setPresence({ game: { name: 'Game', type: "Playing"}});
 
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
