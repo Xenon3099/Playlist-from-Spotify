0b01ca47836a3c29a6b670557da761032027e2c9
Xenon3099 <dmamritesh@gmail.com>
2019-06-19 19:26:14 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,8 +21,6 @@ bot.on('ready', function (evt) {
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
 
-bot.setPresence({ game: { name: 'Game', type: "Playing"}});
-
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `>>`
